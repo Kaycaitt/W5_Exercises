1b5183cef2dae54d8febc58eb0e285a709bd1099
Kaycaitt <caitlantbeard@gmail.com>
2024-10-21 14:32:38 -0500

    Updating code for question 7 as it was computing the type of the variable.

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ print('Hello world!')
 message = 'Hello world!'
 print(message)
 
-dollars = '3'
-cents = '.50'
-print(type(dollars + cents))
+dollars = 3
+cents = .50
+print(dollars + cents)
 
 
 print('3 dollars' + ' ' + 'fifty cents')
