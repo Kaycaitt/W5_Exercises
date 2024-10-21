1d3c64cc507b0b0bf686d18c8af39195aa36f2b5
Kaycaitt <caitlantbeard@gmail.com>
2024-10-21 15:41:18 -0500

    Updating repository to include Exercise 1.B

---
+++ variable_examples.py
@@ -0,0 +1,26 @@
+id = '52364' #string, don't want integer because the number shouldn't be operated on
+
+fullname = 'Rose Marie Cohan' #string, should be text/characters
+
+gender = 'Female' #string, text/characters, I would put Boolean, but there can be so many genders
+
+birthdate = 'February 20 1450' #string, should be text/characters
+
+dlnumber = 'C64289344271' #string, should be text/characters
+
+autopolicynumber = '9781563123' #string, should be text/characters
+
+#Question 4
+myname = 'Caitlan Taylor Beard' #string, should be text/characters
+birthcitystate = 'Chicago, IL' #string, should be text/characters
+
+#Lab 2 Questions 1 & 2
+# Question 1: Full List of Reserved Words 
+# False, None, True, and, as, assert, break, class, continue, def, del, elif, else, except, finally, for, form, global, if, import, in, is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield
+
+# Question 2: Definitions (I chose keywords that I don't have a solid understanding on.)
+# lambda = "An anonymous function. It can take any number of arguments but only have ea single expression."
+# nonlocal = "Used in functions inside functions to create anonymous functions."
+# yield = "Ends a function and returns an ^iterator^"
+# for = "Used to create a ^for^ loop."
+# continue = "Control flow statement used to continue to the next iteration of a loop. The statement does NOT exit the loop like break."
