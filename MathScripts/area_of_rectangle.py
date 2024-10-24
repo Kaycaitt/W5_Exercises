ed66c62a5cec02f168b5a4f391d66dc35b49b1f1
Kaycaitt <caitlantbeard@gmail.com>
2024-10-23 22:27:18 -0500

    Uploading completed question 2 for exercise 2A.

---
+++ MathScripts/area_of_rectangle.py
@@ -0,0 +1,8 @@
+#Question: How do you calculate the area of a rectangle?
+
+base = 14.3354
+height = 20.845789
+
+area_of_rectangle = base * height
+
+print("The area of the rectangle is " + str(format(area_of_rectangle, ".2f")))
