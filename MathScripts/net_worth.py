f3eb394ec215efff4ee480027bf65e240f9f3e77
Kaycaitt <caitlantbeard@gmail.com>
2024-10-23 22:20:21 -0500

    Uploading Question 1 fully completed for exercise 2A.

---
+++ MathScripts/net_worth.py
@@ -0,0 +1,7 @@
+#Question: How do you calculate your net worth given your assets and debts?
+debts = 30000.00
+assets = 100000.00
+
+net_worth = assets - debts
+
+print("Your net worth is " + str(format(net_worth, ".2f")))
