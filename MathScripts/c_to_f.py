a29b236e303b68442d18850d1aa84c9e186ee8bd
Kaycaitt <caitlantbeard@gmail.com>
2024-10-24 08:47:33 -0500

    Uploading completed c_to_f.py exercise.

---
+++ MathScripts/c_to_f.py
@@ -0,0 +1,8 @@
+#Question: How do you convert a temperature from Celsius to Fahrenheit?
+
+celsius = -32
+fahrenheit = 60
+
+convert_to_fahrenheit = (celsius*9/5) + 32
+
+print(format(convert_to_fahrenheit, ".2f") + " degrees Fahrenheit")
