edeaa1d335822bf146845bb2d459266efa9eb92c
Kaycaitt <caitlantbeard@gmail.com>
2024-10-23 22:45:32 -0500

    Question 3 has been completed but I have questions to ask Bess on full code execution.

---
+++ MathScripts/tip_amount.py
@@ -0,0 +1,10 @@
+#Question: How do you calculate the tip amount on a restaurant bill given the tip percentage?
+
+bill_total = 365.98
+tip = 0.15
+
+tip_amount = bill_total * tip
+
+print("The tip on a $" + str(bill_total) + " restaurant bill is $" + str(format(tip_amount, ".2f")))
+
+#code runs when running specific code lines, when trying to run whole file, I get SyntaxError: invalid syntax. Reach out to Bess about this
