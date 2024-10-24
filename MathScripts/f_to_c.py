f03e36fe2fc1e72c80c0b4833201b4b6659dede5
Kaycaitt <caitlantbeard@gmail.com>
2024-10-24 08:28:08 -0500

    Uploading completed f_to_c.py file.

---
+++ MathScripts/f_to_c.py
@@ -0,0 +1,10 @@
+# Question: How do you convert a temperature from Farenheit to Celsius?
+
+Farenheit = 82
+Celsius = 36
+
+convert_to_celsius = ((Farenheit) - 32) * 5/9
+#this is converting given farenheit temperature to celsius
+print(format(convert_to_celsius, ".2f") + " degrees celsius")
+
+#checked with a converter, output is proper answer
