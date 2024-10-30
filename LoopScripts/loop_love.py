506dd4d28e8c17674d2efafa42449f02efce81ff
Kaycaitt <caitlantbeard@gmail.com>
2024-10-30 16:30:14 -0500

    Uploading completed loop_love.py exercise.

---
+++ LoopScripts/loop_love.py
@@ -0,0 +1,4 @@
+list_of_words = ['amazing', 'awesome', 'excellent', 'extraordinary', 'exceptional', 'prominent']
+for loop_list in list_of_words:
+    print(f'''Loops are {loop_list}''')
+print("I <3 loops")
