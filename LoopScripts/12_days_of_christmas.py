da1638c1fa4bb3db407230febd614402478ef7f0
Kaycaitt <caitlantbeard@gmail.com>
2024-10-30 18:20:10 -0500

    Uploading completed 12_days_of_christmas.py exercise. Need to ask Bess to walk me through the syntax for the proper output.

--- LoopScripts/12_days_of_christmas.py
+++ LoopScripts/12_days_of_christmas.py
@@ -1,4 +1,4 @@
-christmas_text = f'''On the first day of Christmas,
+christmas_text = [f'''On the first day of Christmas,
 my true love gave to me
 A partridge in a pear tree.
 On the second day of Christmas,
@@ -99,6 +99,26 @@ Five golden rings,
 Four calling birds,
 Three French hens,
 Two turtle doves,
-And a partridge in a pear tree!'''
+And a partridge in a pear tree!''']
 
-print(christmas_text)
+gifts = [
+    'a partridge in a pear tree',
+    'two turtle doves',
+    'three french hens',
+    'four calling birds',
+    'five golden rings',
+    'six geese a-laying',
+    'seven swans a-swimming',
+    'eight maids a-milking',
+    'nine ladies dancing',
+    'ten lords a-leaping',
+    'eleven pipers piping',
+    'twelve drummers drumming']
+for day in range(12):
+    print(f"On the {day + 1} day of Christmas my true love gave to me:")
+    for gift in range(day, -1, -1):
+        if gift == 0 and day > 0:
+            print("and ", end="")
+        print(gifts[gift], end=', ' if gift > 0 else '.\n')
+
+#I couldn't for the life of me figure out this challenge. I asked chat gpt to help with the print statements to get the proper output.
---
+++ MiniProject/number_guesser.py
@@ -0,0 +1,7 @@
+#hint, need to use at least 2 collection data types in my solution
+
+
+#Question 3: Show higher or lower to help the user get it right.
+
+#Bonus: Keep track of number of guesses, collect all guessed numbers and print at end
+#Bonus2: more than 5 guesses, print "You're awesome" / make program safe for non-numeric input
